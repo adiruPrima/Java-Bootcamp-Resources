7bb60d71a62ee3284feadb7513912125f843adbe
Adil Primaputra <adiru.prima@gmail.com>
2024-06-04 11:48:18 +0700

    finish Workbook 3.5

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 public class WorkSchedule {
     public static void main(String[] args) {
         
-        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+        // See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
-        int day = 3;   //3rd day of the week...
-        boolean holiday = true;
-         
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter day number (e.g Monday => 1): ");
+        int day = scan.nextInt();  
+
+        System.out.print("Enter true or false if it's holiday: ");
+        boolean holiday = scan.nextBoolean();
+
         // IF - ELSE IF - ELSE HERE!
-        
-        
+        if (holiday) {
+            System.out.println("Hell yeah!!!");
+        } else if (day < 6) {
+            System.out.println("I work...");
+        } else {
+            System.out.println("It's weekend, chill..");
         }
-
+        
+        scan.close();
     }
+}
