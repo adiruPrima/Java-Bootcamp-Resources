be2b184fee7e299fec26c86697c115f4676b9fd2
Adil Primaputra <adiru.prima@gmail.com>
2024-06-04 11:27:06 +0700

    finish Workbook 3.4

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
+
 public class WeatherNetwork {
     public static void main(String[] args) {
 
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
-
+        Scanner scan = new Scanner(System.in);
         
-        int temp = 25;  
+        System.out.print("Enter the temperature in Celcius: ");
+        int temp = scan.nextInt();
 
         String forecast = "";
 
         //IF - ELSE IF - ELSE STATEMENTS HERE!
         
+        if (temp < 0) {
+            forecast = "It's freezing balls bruh.. Stay cozy, wear a coat if outside.";
+        } else if (temp <= 10) {
+            forecast = "It's chilly, wear doubles; sweater, jacket, whatever you got.";
+        } else {
+            forecast = "It's a nice weather. Go touch grass!";
+        }
         
         System.out.println(forecast);
+        scan.close();
     }
 }
