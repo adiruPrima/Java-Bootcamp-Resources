16b4500a9d7354ab53c24c1d754049f22e4dc6b0
Adil Primaputra <adiru.prima@gmail.com>
2024-06-13 18:02:14 +0700

    finish Workbook 4.5: area calculator

--- Module 1 - Introduction to Java/4. Functions/Workbook 4.5/AreaCalculator.java
+++ Module 1 - Introduction to Java/4. Functions/Workbook 4.5/AreaCalculator.java
@@ -11,10 +11,14 @@ public class AreaCalculator {
         // double rectangle = area of rectangle with a length of 1, and a width of 2. 
         // double triangle = area of triangle with a base of 1, and a width of 2. 
         // double circle = area of circle with a radius of 2. 
+        double square = areaSquare(2);
+        double rectangle = areaRectangle(1, 2);
+        double triangle = areaTriangle(1, 2);
+        double circle = areaCircle(2);
 
 
         //Task 7 – Call a function that prints all the areas. 
-
+        printAreas(square, rectangle, triangle, circle);
     }
 
     //Task 1 - Write a function that calculates the area of a square. 
@@ -30,8 +34,7 @@ public class AreaCalculator {
      *  2. Calculates the area of the square. A = side²
      */
     public static double areaSquare(double side) {
-
-        return 0;
+        return Math.pow(side, 2);
     }
 
 
@@ -49,8 +52,7 @@ public class AreaCalculator {
      *  2. Calculates the area of the rectangle. A = length * width
      */
     public static double areaRectangle(double length, double width) {
-
-        return 0;  
+        return length * width;  
     } 
 
     //Task 3 - Write a function that calculates the area of a triangle. 
@@ -66,8 +68,7 @@ public class AreaCalculator {
      *  2. Calculates the area of the triangle. A = (base * height)/2
      */ 
     public static double areaTriangle(double base, double height) {
-
-        return 0; 
+        return (base * height) / 2; 
     }
 
     //Task 4 - Write a function that calculates the area of circle. 
@@ -83,8 +84,7 @@ public class AreaCalculator {
      *  2. Calculates the area of the circle. 
      */ 
     public static double areaCircle(double radius) {
-
-        return 0;
+        return 2 * Math.PI * radius;
     }
 
 
@@ -106,6 +106,11 @@ public class AreaCalculator {
      *  4. print: ("Circle area: <circle area>")
      * 
      */
-
+    public static void printAreas(double square, double rectangle, double triangle, double circle) {
+        System.out.println("Square area: " + square);
+        System.out.println("Rectangle area: " + rectangle);
+        System.out.println("Triangle area: " + triangle);
+        System.out.println("Circle area: " + circle);
+    }
 
 }
