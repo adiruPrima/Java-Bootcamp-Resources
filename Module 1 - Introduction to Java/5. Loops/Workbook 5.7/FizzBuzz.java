d6b152389f5cb11bdf27944a4d13720547cb5b07
Adil Primaputra <adiru.prima@gmail.com>
2024-06-14 18:37:13 +0700

    finish Workbook 5.7

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
 
         // See Learn the Part for detailed instructions.
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = scan.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + " - FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println(i + " - Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println(i + " - Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+        scan.close();
 
     }
 
