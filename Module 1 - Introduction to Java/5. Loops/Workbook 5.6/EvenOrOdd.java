87488bd09e69d602a571bc852c6b8f28ac8e7a65
Adil Primaputra <adiru.prima@gmail.com>
2024-06-14 18:19:36 +0700

    finish Workbook 5.6

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class EvenOrOdd {
     public static void main(String[] args) {
         // See Learn the Part for detailed instructions.
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter an integer number: ");
+        int num = scan.nextInt();
+
+        for (int i = 0; i <= num; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " - even");
+            } else {
+                System.out.println(i + " - odd");
+            }
+        }
+        scan.close();
     }
 }
