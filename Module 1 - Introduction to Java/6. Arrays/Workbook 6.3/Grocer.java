524a2fc6a16695fad2d65b36ddb4b78e596e0540
Adil Primaputra <adiru.prima@gmail.com>
2024-06-26 10:11:26 +0700

    finish Workbook 6.3

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.3/Grocer.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.3/Grocer.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
 public class Grocer {
     public static void main(String[] args) {
         
         // Instructions for this workbook are on Learn the Part (Workbook 6.3).
+        String[] aisles = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
 
-        System.out.println("\nDo you sell coffee?");
-        System.out.println("\nWe have that in aisle: <index>");
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("\nSearch item: ");
+        String item = scan.nextLine();
+        item = item.toLowerCase();
+
+        boolean found = false;
+
+        for (int i = 0; i < aisles.length; i++) {
+            if (item.equals(aisles[i])) {
+                System.out.println("\nWe have that in aisle: " + i);
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Sorry, we don't have that here.");
+        }
     
+        scan.close();
     }
 }
