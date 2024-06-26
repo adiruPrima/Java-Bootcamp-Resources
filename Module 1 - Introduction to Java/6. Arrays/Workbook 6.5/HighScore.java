0acea7d5b601b52c9b17fe1245951c1285cb4f93
Adil Primaputra <adiru.prima@gmail.com>
2024-06-26 10:27:48 +0700

    finish Workbook 6.5

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
@@ -2,13 +2,25 @@ public class HighScore {
     public static void main(String[] args) {
         
         int highScore = 0;
+
+        int[] scores = new int[10];
         
         // Instructions for this workbook are on Learn the Part (Workbook 6.5).
 
-        System.out.print("Here are the scores: <score elements>");
+        System.out.print("Here are the scores: ");
+
+        for (int i = 0; i < 10; i++) {
+            scores[i] = rand();
+            System.out.print(scores[i] + " ");
+            if (highScore < scores[i]) highScore = scores[i];
+        }
 
         System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!");
         
     }    
+
+    public static int rand() {
+        return (int)(Math.random() * 50000);
+    }
 
 }
