49da3973c9b94b0259cd1ae77d797e752effc409
Adil Primaputra <adiru.prima@gmail.com>
2024-06-27 12:33:48 +0700

    finish Workbook 6.14

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
@@ -2,6 +2,26 @@ public class RandomNumbers {
     public static void main(String[] args) {
         
         // The instructions for this workbook are on Learn the Part (Workbook 6.14)
+        int[][] numbers = new int[100][10];
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                numbers[i][j] = randomNumber();
+            }
+        }
+
+        print2DArray(numbers);
     }
 
+    public static int randomNumber() {
+        return (int)(Math.random() * 100);
+    }
+
+    public static void print2DArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
