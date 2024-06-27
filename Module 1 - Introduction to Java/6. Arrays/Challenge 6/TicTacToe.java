0c64f02ac0913f7981817cf3ef0e3c8adfbc83ae
Adil Primaputra <adiru.prima@gmail.com>
2024-06-27 22:24:54 +0700

    finish Challenge 6

--- Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
+++ Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
@@ -8,9 +8,10 @@ public class TicTacToe {
             System.out.println("\nLet's play tic tac toe");
 
             //Task 1: Create an array with three rows of '_' characters.
+            String[][] board = {{"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}};
 
             //Task 2: Call the function printBoard();
-
+            printBoard(board);
              
               /*
               {  Task 3: Loop through turns.
@@ -33,6 +34,34 @@ public class TicTacToe {
 
               } 
               */
+            for (int i = 0; i < 9; i++) {
+              int[] choice = new int[2];
+              if (i % 2 == 0) {
+                System.out.println("\nTurn: X");
+                choice = askUser(board);
+                board[choice[0]][choice[1]] = "X";
+              } else {
+                System.out.println("\nTurn: O");
+                choice = askUser(board);
+                board[choice[0]][choice[1]] = "O";
+              }
+              printBoard(board);
+
+              if (checkWin(board) == 3) {
+                System.out.println("X wins!!");
+                System.out.println("---- Game Over ----");
+                break;
+              } else if (checkWin(board) == -3) {
+                System.out.println("O wins!!");
+                System.out.println("---- Game Over ----");
+                break;
+              }
+            }
+
+            if (Math.abs(checkWin(board)) < 3) {
+              System.out.println("Mf it's a TIE!!");
+              System.out.println("---- Game Over ----");
+            }
 
             scan.close();
         }
@@ -48,7 +77,16 @@ public class TicTacToe {
      *      • separate each row by two lines. 
      *      • each row precedes a tab of space
      *      • each character in the grid has one space from the other character
-     */        
+     */  
+    public static void printBoard(String[][] board) {
+      for (int i = 0; i < board.length; i++) {
+        System.out.print("\n\t");
+        for (int j = 0; j < board[i].length; j++) {
+          System.out.print(board[i][j] + " ");
+        }
+        System.out.println("\n");
+      }
+    }      
 
    /** Task 4 - Write a function that lets the user choose a spot
      * Function name – askUser
@@ -61,6 +99,18 @@ public class TicTacToe {
      *   3. Return the row and column in an int[] array.
      * 
      */
+    public static int[] askUser(String[][] board) {
+      int[] choice = new int[2];
+      System.out.print(" - Pick a row and column number: ");
+      choice[0] = scan.nextInt();
+      choice[1] = scan.nextInt();
+      while (!board[choice[0]][choice[1]].equals("_")) {
+        System.out.print(" - Spot taken, try again: ");
+        choice[0] = scan.nextInt();
+        choice[1] = scan.nextInt();
+      }
+      return choice;
+    }
 
     /** Task 6 - Write a function that determines the winner
      * Function name - checkWin 
@@ -74,6 +124,97 @@ public class TicTacToe {
      *   4. Check the left diagonal for a straight X or straight O (Task 9).
      *   5. Check the right diagonal for a straight X or straight O (Task 10).
      */
+    public static int checkWin(String[][] board) {    
+      int rows = checkRows(board); 
+      
+      // Math.abs returns the absolute value of a given number, removing any negative sign. 
+      if (Math.abs(rows) == 3) return rows; // If the block of code consists of only one line, you can omit the curly braces.
+      
+      int columns = checkColumns(board);
+      if (Math.abs(columns) == 3) return columns;  
+      
+      int leftDiagonal = checkLeft(board);
+      if (Math.abs(leftDiagonal) == 3) return leftDiagonal; 
+      
+      int rightDiagonal = checkRight(board);
+      if (Math.abs(rightDiagonal) == 3) return rightDiagonal;
 
+      return 0;
+        
+    }
+  
 
+    public static int checkRows(String[][] board) {
+      int count = 0;
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          if (board[i][j].equals("X")) {
+            count++;
+          } else if (board[i][i].equals("Y")) {
+            count--;
+          }
+        }
+        if (Math.abs(count) == 3) {
+          break;
+        } else {
+          count = 0;
+        }
+      }
+      return count;
+    }
+  
+    public static int checkColumns(String[][] board) {
+      int count = 0;
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          if (board[j][i].equals("X")) {
+            count++;
+          } else if (board[i][i].equals("Y")) {
+            count--;
+          }
+        }
+        if (Math.abs(count) == 3) {
+          break;
+        } else {
+          count = 0;
+        }
+      }
+      return count;
+    }
+      
+    public static int checkLeft(String[][] board) {
+      int count = 0;
+      for (int i = 0; i < 3; i++) {
+        if (board[i][i].equals("X")) {
+          count++;
+        } else if (board[i][i].equals("Y")) {
+          count--;
+        }
+        
+        if (Math.abs(count) == 3) {
+          break;
+        } else {
+          count = 0;
+        }
+      }
+      return count;
+    }
+     
+    public static int checkRight(String[][] board) {
+      int count = 0;
+      for (int i = 0; i < 3; i++) {
+        if (board[2 - i][i].equals("X")) {
+          count++;
+        } else if (board[i][i].equals("Y")) {
+          count--;
+        }
+        
+        if (Math.abs(count) == 3) {
+          break;
+        } else {
+          count = 0;
+        }
+      }
+      return count;
+    }
 }
